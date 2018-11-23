53669e3abeceb39cf92dd81fdad63454c36f3c82
Filip Lindau <filip.lindau@maxlab.lu.se>
2018-11-23 18:37:04 +0100

    modified:   QuadScanTask.py 	modified:   TaskTest.py

--- QuadScanTask.py
+++ QuadScanTask.py
@@ -9,6 +9,7 @@ Tasks for async sequencing.
 import threading
 import uuid
 import logging
+import time
 
 logger = logging.getLogger("Task")
 while len(logger.handlers):
@@ -22,71 +23,131 @@ logger.setLevel(logging.DEBUG)
 
 
 class Task(object):
-    def __init__(self, name=None, task_type="thread", trigger_list=[]):
+    def __init__(self, name=None, task_type="thread", timeout=None, trigger_dict=dict()):
         self.id = uuid.uuid1()
+        if name is None:
+            name = self.id
         self.name = name
-        self.type = task_type
-        self.trigger_list = trigger_list
-        self.trigger_list.append(self.id)
+        self.task_type = task_type
+        self.trigger_dict = dict()
         self.trigger_done_list = list()
-        self.trigger_result_list = list()
-        self.listener_list = list()
+        self.trigger_result_dict = dict()
         self.lock = threading.Lock()
+        self.event_done = threading.Event()
+        self.trigger_event = threading.Event()
         self.started = False
+        self.completed = False
+        self.cancelled = False
+        self.result = None
+        self.timeout = timeout
+        for e in trigger_dict.values():
+            self.add_trigger(e)
 
-    def add_trigger(self, trigger_id):
-        logger.info("{0} {1} adding trigger {2}".format(type(self), self.id, trigger_id))
+    def add_trigger(self, trigger_task):
+        logger.info("{0} {1} adding trigger {2}".format(type(self), self.name, trigger_task.name))
         with self.lock:
-            self.trigger_list.append(trigger_id)
+            self.trigger_dict[trigger_task.id] = trigger_task
 
-    def add_listener(self, listener):
-        logger.info("{0} {1} adding listener {2}".format(type(self), self.id, listener.id))
-        with self.lock:
-            self.listener_list.append(listener)
+    def get_event(self):
+        return self.event_done
 
-    def remove_listener(self, listener):
-        logger.info("{0} {1} removing listener {2}".format(type(self), self.id, listener))
-        with self.lock:
-            if type(listener) == uuid.UUID:
-                for l in self.listener_list:
-                    if l.id == listener:
-                        self.listener_list.remove(l)
-                        break
-            else:
-                try:
-                    self.listener_list.remove(listener)
-                except ValueError:
-                    pass
+    def get_result(self):
+        if self.completed is True:
+            return self.result
+        else:
+            return None
 
     def action(self):
-        pass
+        logger.info("{0} {1} entering action.".format(type(self), self.name))
+        self.result = None
 
-    def emit(self, result):
-        logger.info("{0} {1} done. Emitting signal".format(type(self), self.id))
-        for listener in self.listener_list:
-            listener.update(self.id, result)
+    def emit(self):
+        logger.info("{0} {1} done. Emitting signal".format(type(self), self.name))
+        with self.lock:
+            self.completed = True
+            self.event_done.set()
+
+    def run(self):
+        logger.info("{0} {1} entering run.".format(type(self), self.name))
+
+        # Setup threads for triggers:
+        already_done_flag = True
+        for tr_id in self.trigger_dict:
+            already_done_flag = False
+            th = threading.Thread(target=self._wait_trigger, args=(self.trigger_dict[tr_id],))
+            th.start()
+        if already_done_flag is False:
+            if self.timeout is not None:
+                completed_flag = self.trigger_event.wait(self.timeout)
+                if completed_flag is False:
+                    logger.info("{0} {1} timed out.".format(type(self), self.name))
+                    self.cancel()
+                    return
+            else:
+                self.trigger_event.wait()
+
+        if self.is_cancelled() is True:
+            return
+
+        try:
+            self.action()
+        except Exception as e:
+            self.result = e
+            self.cancel()
+            return
+        self.emit()
 
     def start(self):
-        logger.info("{0} {1} starting.".format(type(self), self.id))
+        logger.debug("{0} {1} starting.".format(type(self), self.name))
         with self.lock:
+            self.trigger_done_list = list()
+            self.trigger_result_dict = dict()
+            self.event_done.clear()
+            self.trigger_event.clear()
             self.started = True
-        self.update(self.id)
-
-    def update(self, task_id, result=None):
-        logger.info("{0} {1} updating trigger_done list.".format(type(self), self.id))
-        with self.lock:
-            if task_id not in self.trigger_done_list:
-                self.trigger_done_list.append(task_id)
-                self.trigger_result_list.append(result)
-        if len(self.trigger_list) == len(self.trigger_done_list):
-            self.action()
+            self.completed = False
+            self.cancelled = False
+            self.result = None
+        t = threading.Thread(target=self.run)
+        t.start()
 
     def cancel(self):
-        logger.info("{0} {1} cancelling.".format(type(self), self.id))
+        logger.debug("{0} {1} cancelling.".format(type(self), self.name))
         with self.lock:
             self.started = False
+            self.completed = False
+            self.cancelled = True
             self.trigger_done_list = list()
-            self.trigger_result_list = list()
+            self.trigger_result_dict = dict()
+            self.trigger_event.set()
+        self.emit()
+
+    def is_cancelled(self):
+        return self.cancelled
+
+    def is_done(self):
+        return self.completed
+
+    def _wait_trigger(self, trigger_task):
+        logger.debug("{0} {1} starting wait for task {2}.".format(type(self), self.name, trigger_task.name))
+        e = trigger_task.get_event()
+        e.wait()
+        # if self.timeout is not None:
+        #     completed_flag = e.wait(self.timeout)
+        #     Cancel task if trigger timed out:
+            # if completed_flag is False:
+            #     self.cancel()
+            #     return
+        # else:
+        #     e.wait()
+        # Cancel task if trigger was cancelled:
+        if trigger_task.is_cancelled() is True:
+            self.cancel()
+            return
+        with self.lock:
+            self.trigger_done_list.append(trigger_task.id)
+            if len(self.trigger_done_list) == len(self.trigger_dict):
+                self.trigger_event.set()
 
     def __eq__(self, other):
         """
@@ -106,15 +167,100 @@ class Task(object):
 
 
 class DelayTask(Task):
-    def __init__(self, delay, name=None):
-        Task.__init__(self, name)
+    def __init__(self, delay, name=None, trigger_dict=dict()):
+        Task.__init__(self, name, trigger_dict=trigger_dict)
         self.delay = delay
 
     def action(self):
-        delayed_call = threading.Timer(self.delay, self.emit, [None])
-        delayed_call.start()
+        logger.info("{0} {1} entering action. Starting delay of {2}s".format(type(self), self.name, self.delay))
+        time.sleep(self.delay)
+        self.result = True
+
+
+class TangoDeviceConnectTask(Task):
+    def __init__(self, device, name=None, timeout=None, trigger_dict=dict()):
+        Task.__init__(self, name, timeout=timeout, etrigger_dict=trigger_dict)
+        self.device = device
+
+    def action(self):
+        logger.info("{0} {1} entering action. ".format(type(self), self.name))
+        self.result = True
+
+
+class TangoReadAttributeTask(Task):
+    def __init__(self, device, attribute, name=None, timeout=None, trigger_dict=dict()):
+        Task.__init__(self, name, timeout=timeout, etrigger_dict=trigger_dict)
+        self.device = device
+        self.attribute = attribute
+
+    def action(self):
+        logger.info("{0} {1} entering action. ".format(type(self), self.name))
+        self.result = True
+
+
+class TangoWriteAttributeTask(Task):
+    def __init__(self, device, attribute, name=None, timeout=None, trigger_dict=dict()):
+        Task.__init__(self, name, timeout=timeout, etrigger_dict=trigger_dict)
+        self.device = device
+        self.attribute = attribute
+
+    def action(self):
+        logger.info("{0} {1} entering action. ".format(type(self), self.name))
+        self.result = True
+
+
+class TangoMonitorAttributeTask(Task):
+    def __init__(self, device, attribute, name=None, timeout=None, trigger_dict=dict()):
+        Task.__init__(self, name, timeout=timeout, etrigger_dict=trigger_dict)
+        self.device = device
+        self.attribute = attribute
+
+    def action(self):
+        logger.info("{0} {1} entering action. ".format(type(self), self.name))
+        self.result = True
+
+
+class ReadImageTask(Task):
+    def __init__(self, image_name, name=None, timeout=None, trigger_dict=dict()):
+        Task.__init__(self, name, timeout=timeout, etrigger_dict=trigger_dict)
+        self.image_name = image_name
+
+    def action(self):
+        logger.info("{0} {1} entering action. ".format(type(self), self.name))
+        self.result = True
+
+
+class ProcessImageTask(Task):
+    def __init__(self, image, name=None, timeout=None, trigger_dict=dict()):
+        Task.__init__(self, name, timeout=timeout, etrigger_dict=trigger_dict)
+        self.image = image
+
+    def action(self):
+        logger.info("{0} {1} entering action. ".format(type(self), self.name))
+        self.result = True
+
+
+class ScanTask(Task):
+    def __init__(self, scan_params, name=None, timeout=None, trigger_dict=dict()):
+        Task.__init__(self, name, timeout=timeout, etrigger_dict=trigger_dict)
+        self.scan_params = scan_params
+
+    def action(self):
+        logger.info("{0} {1} entering action. ".format(type(self), self.name))
+        self.result = True
 
 
 if __name__ == "__main__":
-    t = DelayTask(1.0)
-    t.start()
+    t1 = DelayTask(2.0, name="task1")
+    t2 = DelayTask(1.0, name="task2", trigger_dict={"delay": t1})
+    t3 = DelayTask(0.5, name="task3", trigger_dict={"delay1": t2, "delay2": t1})
+    t4 = DelayTask(0.1, name="task4")
+    t3.add_trigger(t4)
+    t2.timeout = 1.0
+    t4.start()
+    time.sleep(0.5)
+    t3.start()
+    t2.start()
+    t1.start()
+
+    # t2.cancel()
--- TaskTest.py
+++ TaskTest.py
@@ -22,80 +22,31 @@ logger.addHandler(fh)
 logger.setLevel(logging.DEBUG)
 
 
-# From: https://stackoverflow.com/questions/12317940/python-threading-can-i-sleep-on-two-threading-events-simultaneously
-
-def or_set(self):
-    self._set()
-    self.changed()
-
-
-def or_clear(self):
-    self._clear()
-    self.changed()
-
-
-def orify(e, changed_callback):
-    try:
-        callback_list = e.callback_list
-    except AttributeError:
-        callback_list = list()
-        e._set = e.set
-        e._clear = e.clear
-    callback_list.append(changed_callback)
-
-    def changed():
-        [callback() for callback in callback_list]
-
-    e.changed = changed
-    e.set = lambda: or_set(e)
-    e.clear = lambda: or_clear(e)
-
-
-def make_or_event(or_event, *events):
-
-    # or_event = threading.Event()
-    def changed():
-        bools = [e.is_set() for e in events]
-        if any(bools):
-            or_event.set()
-        else:
-            or_event.clear()
-    for e in events:
-        orify(e, changed)
-    changed()
-    return or_event
-
-
 class Task(object):
     def __init__(self, name=None, task_type="thread", timeout=None, trigger_dict=dict()):
         self.id = uuid.uuid1()
         if name is None:
             name = self.id
         self.name = name
-        self.type = task_type
+        self.task_type = task_type
         self.trigger_dict = dict()
         self.trigger_done_list = list()
-        self.trigger_result_list = list()
-        self.listener_list = list()
+        self.trigger_result_dict = dict()
         self.lock = threading.Lock()
         self.event_done = threading.Event()
-        self.cancel_event = threading.Event()
         self.trigger_event = threading.Event()
-        make_or_event(self.trigger_event, self.cancel_event)
         self.started = False
         self.completed = False
         self.cancelled = False
         self.result = None
         self.timeout = timeout
-        for e in trigger_dict.itervalues():
+        for e in trigger_dict.values():
             self.add_trigger(e)
 
     def add_trigger(self, trigger_task):
-        logger.info("{0} {1} adding trigger {2}".format(type(self), self.name, trigger_task.id))
-        e = trigger_task.get_event()
+        logger.info("{0} {1} adding trigger {2}".format(type(self), self.name, trigger_task.name))
         with self.lock:
-            self.trigger_dict[trigger_task.id] = e
-            make_or_event(self.trigger_event, e)
+            self.trigger_dict[trigger_task.id] = trigger_task
 
     def get_event(self):
         return self.event_done
@@ -118,58 +69,85 @@ class Task(object):
 
     def run(self):
         logger.info("{0} {1} entering run.".format(type(self), self.name))
-        while len(self.trigger_done_list) != len(self.trigger_dict):
+
+        # Setup threads for triggers:
+        already_done_flag = True
+        for tr_id in self.trigger_dict:
+            already_done_flag = False
+            th = threading.Thread(target=self._wait_trigger, args=(self.trigger_dict[tr_id],))
+            th.start()
+        if already_done_flag is False:
             if self.timeout is not None:
                 completed_flag = self.trigger_event.wait(self.timeout)
                 if completed_flag is False:
-
+                    logger.info("{0} {1} timed out.".format(type(self), self.name))
                     self.cancel()
                     return
             else:
                 self.trigger_event.wait()
 
-            for tr_id in self.trigger_dict:
-                if tr_id not in self.trigger_done_list:
-                    task = self.trigger_dict[tr_id]
-                    if task.is_cancelled() is True:
-                        self.cancel()
-                        return
-                    self.trigger_done_list.append(tr_id)
-        self.action()
+        if self.is_cancelled() is True:
+            return
+
+        try:
+            self.action()
+        except Exception as e:
+            self.result = e
+            self.cancel()
+            return
         self.emit()
 
     def start(self):
-        logger.info("{0} {1} starting.".format(type(self), self.name))
+        logger.debug("{0} {1} starting.".format(type(self), self.name))
         with self.lock:
+            self.trigger_done_list = list()
+            self.trigger_result_dict = dict()
+            self.event_done.clear()
+            self.trigger_event.clear()
             self.started = True
             self.completed = False
             self.cancelled = False
+            self.result = None
         t = threading.Thread(target=self.run)
         t.start()
 
-    def update(self, task_id, result=None):
-        logger.info("{0} {1} updating trigger_done list.".format(type(self), self.name))
-
-        with self.lock:
-            if task_id not in self.trigger_done_list:
-                self.trigger_done_list.append(task_id)
-                self.trigger_result_list.append(result)
-        if len(self.trigger_dict) == len(self.trigger_done_list):
-            self.action()
-
     def cancel(self):
-        logger.info("{0} {1} cancelling.".format(type(self), self.name))
+        logger.debug("{0} {1} cancelling.".format(type(self), self.name))
         with self.lock:
             self.started = False
             self.completed = False
             self.cancelled = True
             self.trigger_done_list = list()
-            self.trigger_result_list = list()
-            self.cancel_event.set()
+            self.trigger_result_dict = dict()
+            self.trigger_event.set()
         self.emit()
 
     def is_cancelled(self):
         return self.cancelled
+
+    def is_done(self):
+        return self.completed
+
+    def _wait_trigger(self, trigger_task):
+        logger.debug("{0} {1} starting wait for task {2}.".format(type(self), self.name, trigger_task.name))
+        e = trigger_task.get_event()
+        e.wait()
+        # if self.timeout is not None:
+        #     completed_flag = e.wait(self.timeout)
+        #     Cancel task if trigger timed out:
+            # if completed_flag is False:
+            #     self.cancel()
+            #     return
+        # else:
+        #     e.wait()
+        # Cancel task if trigger was cancelled:
+        if trigger_task.is_cancelled() is True:
+            self.cancel()
+            return
+        with self.lock:
+            self.trigger_done_list.append(trigger_task.id)
+            if len(self.trigger_done_list) == len(self.trigger_dict):
+                self.trigger_event.set()
 
     def __eq__(self, other):
         """
@@ -200,7 +178,16 @@ class DelayTask(Task):
 
 
 if __name__ == "__main__":
-    t1 = DelayTask(1.0, name="task1")
-    t2 = DelayTask(2.0, name="task2", trigger_dict={"delay": t1})
+    t1 = DelayTask(2.0, name="task1")
+    t2 = DelayTask(1.0, name="task2", trigger_dict={"delay": t1})
+    t3 = DelayTask(0.5, name="task3", trigger_dict={"delay1": t2, "delay2": t1})
+    t4 = DelayTask(0.1, name="task4")
+    t3.add_trigger(t4)
+    t2.timeout = 1.0
+    t4.start()
+    time.sleep(0.5)
+    t3.start()
     t2.start()
     t1.start()
+
+    # t2.cancel()
