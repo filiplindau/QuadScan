991403c5febab3059cf93c678743f00d803be7d4
Filip Lindau <filip.maxlab@gmail.com>
2021-03-08 18:07:07 +0100

    modified:   QuadScanTasks.py

--- QuadScanTasks.py
+++ QuadScanTasks.py
@@ -2125,11 +2125,11 @@ class PopulateDeviceListTask(Task):
                 # If name and/or position for the screen is not retrievable we cannot use it:
                 except IndexError as e:
                     self.logger.exception("Index error when parsing screen {0}. "
-                                          "This screen will not be included in the populated list. ".format(mag_name))
+                                          "This screen will not be included in the populated list. ".format(sc_name))
                     pass
                 except KeyError as e:
                     self.logger.exception("Key error when parsing screen {0}"
-                                          "This screen will not be included in the populated list. ".format(mag_name))
+                                          "This screen will not be included in the populated list. ".format(sc_name))
                     pass
 
             sect_quads[s] = quad_list
